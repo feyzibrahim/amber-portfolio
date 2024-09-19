a929ebc1c755dfb627d3a12a8a51167325add630
Feyz Ibrahim <46452412+feyzibrahim@users.noreply.github.com>
2024-09-20 04:37:00 +0530

    changed to and from

--- src/server/send-mail.ts
+++ src/server/send-mail.ts
@@ -2,11 +2,11 @@
 import { Resend } from "resend";
 
 export default async function sendMail(sender: string, body: string) {
-	const resend = new Resend(process.env.MINE);
+	const resend = new Resend(process.env.RESEND_API_KEY);
 
 	await resend.emails.send({
 		to: process.env.AMBER_EMAIL as string,
-		from: "no-replay@exiphones.shop",
+		from: "no-replay@basys.ai",
 		subject: `Message From: ${sender}`,
 		html: `<p>${body}</p>`,
 	});
